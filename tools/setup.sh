273e2a6f839a0a163663d5f2d60c28b0f219714a
Venkatavaradhan Vembanoor Lakshmi Narayanan <venkatavaradhan93@gmail.com>
2022-03-15 23:11:29 -0700

    Building robot image in CI (#31)
    * Building robot image in CI
    
    * sdk base image finally works

--- tools/setup.sh
+++ tools/setup.sh
@@ -1,21 +1,22 @@
 #!/bin/sh
 
 TOOLS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+WORKSPACE=${HOME}/beertender_robot
 
 function beertender () {
     python3 $TOOLS_DIR/beertender_main.py $@
 }
 
 function sdk () {
-    docker run -it --rm --net=host --privileged \
-        -e DISPLAY=$DISPLAY \
-        -e TERM \
-        -e QT_X11_NO_MITSHM=1\
-        -e XAUTHORITY=/tmp/.docker.xauth \
-        -v /tmp/.docker.xauth:/tmp/.docker.xauth \
-        -v /tmp/.X11-unix/:/tmp/.X11-unix \
+    docker run --rm \
+        --runtime=nvidia \
+        -e "DISPLAY=${DISPLAY:-:0.0}" \
+        -v /tmp/.X11-unix:/tmp/.X11-unix \
+        -v $HOME/.Xauthority:/root/.Xauthority \
+        -v $HOME/beertender_robot:/home/user/beertender_robot:rw \
+        --device /dev/input/js0 \
+        --security-opt apparmor:unconfined \
+        --net=host \
         --gpus all \
-        --runtime nvidia \
-        --platform linux/arm64 \
-        sdk-base:latest
+        -it sdk-base:latest
 }
